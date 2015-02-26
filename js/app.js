14ce2739a17f96e8bcf707508a13cc789a3d3a02
Benjamin Nold <brnold@oakland.edu>
2015-02-26 09:32:23 -0500

    Created a few buttons. Finally
    I think I understand now how ember.js works.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
 App = Ember.Application.create(); 
 
 App.Router.map(function() {
-  // put your routes here
+  //this.resouce('index', {path: '/'});
+});
+
+App.ApplicationController = Ember.Controller.extend({
+	firstName: "Frank",
+	lastName: "Letzes Name",
+
+	submitForm: function() {
+      // Get the text from the textfields
+      var firstName = this.get('firstNameTextBox');
+      var lastName = this.get('lastNameTextBox');
+      var DOB = this.get('dobTextBox');
+      
+      if (!title.trim()) { return; } //if there is text don't do nothing
+
+      // Create the new Todo model
+      var text = this.store.createRecord('text', {
+        title: title,
+      });
+
+      // Clear the "New Todo" text field
+      this.set('textBox', '');
+
+      // Save the new model
+      text.save();
+    },
+
+    
 });
 
 App.IndexRoute = Ember.Route.extend({
