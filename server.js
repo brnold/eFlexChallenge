70e197cba809db31c84458f8aec910006553b132
Benjamin Nold <brnold@oakland.edu>
2015-02-27 10:30:59 -0500

    Added the node.js webserver
    I am using express. The server code is not entirely my own. It’s a line
    from here, a line from there. Majority came from stack overflow.

---
+++ server.js
@@ -0,0 +1,22 @@
+
+var express = require('express'); 
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname })
+
+});
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css',express.static(__dirname + '/css'));
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
+
+
